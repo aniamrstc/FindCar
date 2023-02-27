eddb807b88a0e922c31ee1c058fce7d5ead689c6
lilianafss <liliana.sntss@eduge.ch>
2023-02-27 10:39:00 +0100

    header page accueil

---
+++ src/public/vue/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/865258096d.js" crossorigin="anonymous"></script>
+    <title>Accueil</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="../../assets/LogoMini.PNG" alt="" width="50" height="50">
+            </a>
+            <a class="d-flex" href="#">
+                <i class="fa-solid fa-user"></i>
+            </a>
+        </div>
+    </nav>
+    <div class="container">
+        <form action="">
+            <div class="col-sm-12 selectVehicle">
+                <p>
+                    Quel type de véhicule ?
+                </p>
+
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-primary">Voiture</button>
+                    <button type="button" class="btn btn-primary">Utilitaires</button>
+                    <button type="button" class="btn btn-primary">Prestige</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+
+</body>
+
+</html>
