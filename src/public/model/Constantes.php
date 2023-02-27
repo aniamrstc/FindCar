cdd53790ec04554c51df0ee4194350b3d4b85511
aniamrstc <ania.mrstc@eduge.ch>
2023-02-27 08:58:31 +0100

    dossier vue

---
+++ src/public/model/Constantes.php
@@ -0,0 +1,10 @@
+<?php 
+// projet:Realiser un blog
+// Auteur:Ania Marostica
+// Date:02.02.2023
+// Description:page des constante de connexion 
+
+const DB_HOST = 'localhost';
+const DB_NAME = "BDDFindCar";
+const DB_USER = 'FindCar';
+const DB_PASSWORD = 'Find306Car';
