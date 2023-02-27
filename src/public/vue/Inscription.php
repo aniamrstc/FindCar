7be4948e311966803a00acb5b97a950a30617446
aniamrstc <ania.mrstc@eduge.ch>
2023-02-27 11:38:58 +0100

    style inscription

---
+++ src/public/vue/Inscription.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/865258096d.js" crossorigin="anonymous"></script>
+    <title>Inscription</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="../../assets/LogoMini.PNG" alt="" width="50" height="50">
+            </a>
+            <a class="d-flex" href="#">
+                <i class="fa-solid fa-user"></i>
+            </a>
+        </div>
+    </nav>
+    <div>
+        <div class="card-body p-4 p-md-5">
+            <h2 class="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">Inscription </h2>
+            <form method="POST" class="px-md-2">
+
+            <div class="form-outline mb-4">
+                <input type="text" name="email" id="email" placeholder="Email" class="form-control">
+            </div>
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        
+                        <input type="password" name="password" id="password" placeholder="Mot de passe" class="form-control">
+                    </div>
+                    <div class="col-md-6 mb-4">
+                        <input type="password" name="Retypepassword" id="Retypepassword" placeholder=" Confirmer le mot de passe" class="form-control">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <input type="text" name="numPermis" id="NumPermis" placeholder="Numéro de permis" class="form-control">
+                    </div>
+                    <div class="col-md-6 mb-4">
+                        <input type="date" name="date" id="date" placeholder="Date de naissance " class="form-control">
+                    </div>
+                </div>
+                <div class="form-outline mb-4">
+                <input type="submit" name="inscription" id="Inscription" value="Inscription"class="form-control btn btn-primary" >
+                </div>
+                <div class="d-flex justify-content-center align-items-center">
+                <p>Ou</p>
+                </div>
+                <div class="d-flex justify-content-center align-items-center">
+                <input type="submit" name="connexion" id="Connexion" value="Connexion" class="btn btn-primary col-6">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
