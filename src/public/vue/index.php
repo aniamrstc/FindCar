2d52b5b0b4b9aa067b5546593d3dcd1998d539c1
lilianafss <liliana.sntss@eduge.ch>
2023-03-06 08:16:26 +0100

    style accueil

--- src/public/vue/index.php
+++ src/public/vue/index.php
@@ -7,6 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
     <script src="https://kit.fontawesome.com/865258096d.js" crossorigin="anonymous"></script>
     <title>Accueil</title>
 </head>
@@ -15,30 +23,84 @@
     <nav class="navbar navbar-dark bg-dark">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="../../assets/LogoMini.PNG" alt="" width="50" height="50">
+                <img src="../../assets/LogoMiniNom-removebg-preview.png" alt="" width="150" height="50">
             </a>
             <a class="d-flex" href="#">
                 <i class="fa-solid fa-user"></i>
             </a>
         </div>
     </nav>
-    <div class="container">
+    <div class="container mt-5">
         <form action="">
             <div class="col-sm-12 selectVehicle">
                 <p>
                     Quel type de véhicule ?
                 </p>
 
-                <div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" class="btn btn-primary">Voiture</button>
-                    <button type="button" class="btn btn-primary">Utilitaires</button>
-                    <button type="button" class="btn btn-primary">Prestige</button>
+                <div class="btn-group mb-3" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-primary"><i class="fa-solid fa-car-side"></i> Voiture</button>
+                    <button type="button" class="btn btn-primary"><i class="fa-solid fa-truck"></i> Utilitaires</button>
+                    <button type="button" class="btn btn-primary"><i class="fa-solid fa-car-side"></i> Prestige</button>
                 </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <label for="lieuDepart">Lieu de départ :</label>
+                    <input type="text" class="form-control" name="lieuDepart">
+                </div>
+                <div class="col">
+                    <label for="lieuRetour">Lieu de retour :</label>
+                    <input type="password" class="form-control" name="lieuRetour">
+                </div>
+
+            </div>
+            <div class="row mt-3">
+                <div class="col">
+                    <label for="email">Date de départ :</label>
+                    <input type="date" name="date" id="date" placeholder="Date de naissance " class="form-control">
+                </div>
+                <div class="col">
+                    <label for="email">Date de retour :</label>
+                    <input type="date" name="date" id="date" placeholder="Date de naissance " class="form-control">
+                </div>
+            </div>
+            <div>
+                <input class="btn btn-primary mt-3 float-end" type="submit" value="Rechercher">
             </div>
         </form>
     </div>
+    <footer class="text-center text-white fixed-bottom bg-dark">
 
+        <div class="container p-4">
+            <img src="../../assets/LogoMiniNom-removebg-preview.png" width="150" height="50">
+        </div>
+        <section class="mb-4">
+            <!-- Facebook -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                    class="fab fa-facebook-f"></i></a>
 
+            <!-- Twitter -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+
+            <!-- Google -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+
+            <!-- Instagram -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                    class="fab fa-instagram"></i></a>
+
+            <!-- Linkedin -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                    class="fab fa-linkedin-in"></i></a>
+
+            <!-- Github -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
+        </section>
+        <div class="text-center p-3 bg-dark">
+            © 2020 Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
+    </footer>
 </body>
 
 </html>
