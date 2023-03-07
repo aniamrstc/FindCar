e2f2dfd876399a139c36bc6326b84eb5356066d3
lilianafss <liliana.sntss@eduge.ch>
2023-03-07 14:12:40 +0100

    update inscription et commentaires

--- src/public/vue/inscription.php
+++ src/public/vue/inscription.php
@@ -1,3 +1,8 @@
+<!-- 
+    Projet : FindCar
+    Auteur : Liliana Santos
+    Date : 06.03.2023
+ -->
 <?php
 require("../model/BDD.php");
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
@@ -7,7 +12,19 @@ $numPermis = filter_input(INPUT_POST, 'numPermis', FILTER_SANITIZE_SPECIAL_CHARS
 $date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_SPECIAL_CHARS);
 $inscription = filter_input(INPUT_POST, 'inscription', FILTER_SANITIZE_SPECIAL_CHARS);
 $connexion = filter_input(INPUT_POST, 'connexion', FILTER_SANITIZE_SPECIAL_CHARS);
+
+/* Stocker la date de aujord'hui */
+$dateActuelle = new DateTime();
+
+/* stocker la date inserer par l'utilisateur . */
+$dateNaissance = new DateTime($date);
+
+/* Calcul de l'âge de l'utilisateur. */
+$age = $dateActuelle->diff($dateNaissance)->y;
+
 $messageError = "";
+
+/* Vérifier si l'e-mail est déjà dans la base de données. */
 $userExists = userExists($email);
 
 if (isset($connexion)) {
@@ -15,16 +32,44 @@ if (isset($connexion)) {
 }
 
 if (isset($inscription)) {
+    /* Vérifier si l'email, le mot de passe, le mot de passe2, numPermis et la date ne sont pas vides. */
     if ($email != "" && $password != "" && $password2 != "" && $numPermis != "" && $date != "") {
+        /* Vérifier si le mot de passe et le mot de passe retapé sont identiques. */
         if ($password == $password2) {
+
+            /* Vérifier si l'e-mail existe dans la base de données. */
             if ($userExists['email_exists'] === 1) {
-                $messageError = "Cette adresse mail est déjà utilisée.";
+                $messageError .= nl2br("Cette adresse mail est déjà utilisée. \n");
             } else {
                 $passwordHash = password_hash($password, PASSWORD_BCRYPT);
-                newUser($email, $passwordHash, $numPermis, $date);
+                /* Vérifier si l'utilisateur a plus de 18 ans. */
+                if ($age > 18) {
+                    newUser($email, $passwordHash, $numPermis, $date);
+                } else {
+                    $messageError .= nl2br("Vous devez etre majeur pour vous inscrire \n");
+                }
             }
+        } else {
+            $messageError .= nl2br("Les mots de passe ne sont pas compatibles \n");
         }
     }
+
+    if (empty($email)) {
+        $messageError .= nl2br("Veuillez indiquer votre email \n");
+    }
+    if (empty($password)) {
+        $messageError .= nl2br("Veuillez indiquer votre mot de passe \n");
+    }
+    if (empty($password2)) {
+        $messageError .= nl2br("Veuillez confirmer votre mot de passe \n");
+    }
+    if (empty($numPermis)) {
+        $messageError .= nl2br("Veuillez indiquer votre numero de permis \n");
+    }
+    if (empty($date)) {
+        $messageError .= nl2br("Veuillez indiquer votre date de naissance \n");
+    }
+
 }
 ?>
 
@@ -60,28 +105,34 @@ if (isset($inscription)) {
             <form method="POST" class="px-md-2 " style="margin:50px 150px">
 
                 <div class="form-outline mb-4">
-                    <input type="text" name="email" id="email" placeholder="Email" class="form-control">
+                    <label for="email">Votre email :</label>
+                    <input type="text" name="email" id="email" placeholder="Email" class="form-control" required>
                 </div>
                 <div class="row">
                     <div class="col-md-6 mb-2">
-
-                        <input type="password" name="password" id="password" placeholder="Mot de passe"
-                            class="form-control">
+                        <label for="password">Votre mot de passe :</label>
+                        <input type="password" name="password" id="password" placeholder="8 caractères min."
+                            class="form-control" minlength="8">
                     </div>
                     <div class="col-md-6 mb-2">
-                        <input type="password" name="Retypepassword" id="Retypepassword" placeholder=" Confirmer le mot de passe" class="form-control">
+                        <label for="Retypepassword">Confirmer le mot de passe :</label>
+                        <input type="password" name="Retypepassword" id="Retypepassword" placeholder="8 caractères min."
+                            class="form-control" minlength="8">
                     </div>
-                    <label for="showPassWord" id="showMyPassword">
-                        <input type="checkbox" name="showMyPassword" id="showMyPassword" onclick="ShowMyPassword()">
+                    <div for="showPassWord" id="showMyPassword" class="d-flex justify-content-end">
+                        <input type="checkbox" name="showMyPassword" id="showMyPassword" onclick="ShowMyPassword()"
+                            style="margin-right: 5px;">
                         <span class="ml-2">Afficher le mot de passe</span>
-                    </label>
+                    </div>
                 </div>
                 <div class="row mt-3">
                     <div class="col-md-6 mb-4">
+                        <label for="Retypepassword">Numéro du permis :</label>
                         <input type="text" name="numPermis" id="numPermis" placeholder="Numéro de permis"
                             class="form-control">
                     </div>
                     <div class="col-md-6 mb-4">
+                        <label for="Retypepassword">Date de naissance :</label>
                         <input type="date" name="date" id="date" placeholder="Date de naissance " class="form-control">
                     </div>
                 </div>
@@ -98,6 +149,19 @@ if (isset($inscription)) {
                 </div>
             </form>
         </div>
+
+        <?php
+        if (!empty($messageError)) {
+            if ($messageError != 0) { ?>
+                <div class="row h-100 justify-content-center align-items-center">
+                    <div class="alert alert-danger w-50 mt-3 col-12 text-center" role="alert">
+                        <?= $messageError ?>
+                    </div>
+                </div>
+            <?php }
+        }
+        ?>
+
     </div>
 </body>
 
